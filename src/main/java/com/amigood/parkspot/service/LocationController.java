e9b7f18a5467011e2ba167565d912ddf5e1f52e7
Leo Amigood <1stdibs@amigood.com>
2012-08-15 18:54:47 -0400

    Started location service development

---
+++ src/main/java/com/amigood/parkspot/service/LocationController.java
@@ -0,0 +1,21 @@
+package com.amigood.parkspot.service;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+
+/**
+ * User: leo@amigood.com | Leo Amigood
+ * Date: 8/15/12
+ * Time: 4:48 PM
+ */
+@Controller
+public class LocationController {
+
+    @RequestMapping(method=RequestMethod.GET, value="/location/{coordinates}")
+    public String getAddress(@PathVariable String coordinates) {
+        return coordinates;
+    }
+
+}
---
+++ src/test/java/com/amigood/parkspot/service/LocationControllerTest.java
@@ -0,0 +1,44 @@
+package com.amigood.parkspot.service;
+
+import org.junit.Before;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.mock.web.MockHttpServletRequest;
+import org.springframework.mock.web.MockHttpServletResponse;
+import org.springframework.test.context.ContextConfiguration;
+import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
+import org.springframework.web.servlet.mvc.annotation.AnnotationMethodHandlerAdapter;
+
+/**
+ * User: leo@amigood.com | Leo Amigood
+ * Date: 8/15/12
+ * Time: 4:59 PM
+ */
+@RunWith(SpringJUnit4ClassRunner.class)
+@ContextConfiguration("classpath:/conf/spring-test.xml")
+public class LocationControllerTest {
+
+    MockHttpServletRequest requestMock;
+    MockHttpServletResponse responseMock;
+    AnnotationMethodHandlerAdapter handlerAdapter;
+
+    @Autowired
+    LocationController controller;
+
+    @Before
+    public void setUp() {
+        requestMock = new MockHttpServletRequest();
+        responseMock = new MockHttpServletResponse();
+        handlerAdapter = new AnnotationMethodHandlerAdapter();
+    }
+
+    @Test
+    public void testGetAddress() throws Exception {
+        requestMock.setMethod("GET");
+        requestMock.setRequestURI("/location/40.697488,-73.979681");
+
+        handlerAdapter.handle(requestMock, responseMock, controller);
+    }
+
+}
